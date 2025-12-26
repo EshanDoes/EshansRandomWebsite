b26fab3a72b34b0473985d572a2125092f3499fe
EshanDoes <eshan.shiraz@gmail.com>
2025-12-27 02:12:34 +0500

    Finished the other websites page! I think this is good enough to promote now, but I'll definitely still be improving it as time goes on.

--- otherwebsites/script.js
+++ otherwebsites/script.js
@@ -4,10 +4,7 @@
 const apiUrl = 'https://pagering.gideon.sh/api/v1/members';
 
 const requestOptions = {
-    method: 'GET',
-    headers: {
-        'Access-Control-Allow-Origin': 'https://pagering.gideon.sh'
-}
+    method: 'GET'
 };
 
 // Make a GET request
@@ -16,11 +13,20 @@ fetch(apiUrl, requestOptions)
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
+
     return response.json();
   })
   .then(data => {
     console.log(data);
+
+    for(let i = 0; i < data.length; i++) {
+        let buttonGroup = document.getElementById("miniButtons");
+        let item = data[i];
+        buttonGroup.innerHTML += '<a href="'+item.url+'"><img src="'+item.buttonUrl+'" alt="'+item.id+'" title="'+item.name+'" /></a>';
+        console.log(item);
+    };
   })
   .catch(error => {
     console.error('Error:', error);
   });
+
